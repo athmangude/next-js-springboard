0f4c2fb2daf244632e622a4bbd9239e4ecd1045a
Athman Gude <athmangude@gmail.com>
2019-12-14 11:39:53 +0300

    added layouts folder

---
+++ layouts/top-navigation-bar-layout/index.js
@@ -0,0 +1,36 @@
+import Head from 'next/head';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+export default ({ children, title="Springboard" }) => {
+    console.log('[rendering top navigation bar layout]');
+    return (
+        <div className="top-navigation-bar-layout">
+            <Head>
+                <title>{title}</title>
+                <meta charSet="utf-8" />
+                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            </Head>
+            <header className="navigation-bar">
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton edge="start" color="inherit" aria-label="menu">
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6">
+                            News
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </header>
+            <div className="content">
+                {children}
+            </div>
+        </div>
+    )
+}
