0c3d8f462345e4474c1938db7727107163afc4c0
Athman Gude <athmangude@gmail.com>
2019-12-14 11:39:36 +0300

    added material ui theme

---
+++ config/theme.js
@@ -0,0 +1,38 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+// Create a theme instance.
+const theme = createMuiTheme({
+    palette: {
+        primary: {
+            main: '#556cd6',
+        },
+        secondary: {
+            main: '#19857b',
+        },
+        error: {
+            main: red.A400,
+        },
+        background: {
+            default: '#fff',
+        },
+    },
+    props: {
+        MuiTypography: {
+            variantMapping: {
+                h1: 'h1',
+                h2: 'h2',
+                h3: 'h3',
+                h4: 'h4',
+                h5: 'h5',
+                h6: 'h6',
+                subtitle1: 'h2',
+                subtitle2: 'h3',
+                body1: 'span',
+                body2: 'span',
+            },
+        },
+    },
+});
+
+export default theme;
